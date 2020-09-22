819b116e695b820cf4fe7cce5b1411296e79917b
francepants <franceline90@gmail.com>
2020-09-22 09:24:08 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,7 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map((array, i) => <MovieCard key={i} {...array}/>)  
   }
 
   render() {
@@ -16,3 +17,5 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+//
